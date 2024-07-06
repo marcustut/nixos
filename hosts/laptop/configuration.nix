7d661f409f15f71d3ea8bb47dde86acfac5e54ca
marcustut <marcustutorial@hotmail.com>
2024-07-07 00:18:21 +0800

    feat: updated for laptop nix-ld

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,5 +1,14 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
 {
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  environment.systemPackages = [
+    pkgs.wget
+  ];
+
+  programs.nix-ld = {
+    enable = true;
+    package = pkgs.nix-ld-rs;
+  };
 }
