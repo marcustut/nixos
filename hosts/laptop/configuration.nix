0181a812617a9f5552d50b141bc293ce3ceec0e1
marcustut <marcustutorial@hotmail.com>
2025-04-29 15:34:39 +0800

    feat: updated laptop nixos config

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,26 +1,28 @@
 { pkgs, config, ... }:
 
-let
-  unstable = import
-    (builtins.fetchTarball {
-      url = "https://github.com/nixos/nixpkgs/tarball/nixos-unstable";
-      sha256 = "sha256:0b1sxjghybf0fis20i9vjg79xwrjw597437v87clva97ghgbf3l7";
-    })
-    {
-      system = pkgs.system;
-      config = config.nixpkgs.config;
-    };
-in
+# let
+#   unstable = import
+#     (builtins.fetchTarball {
+#       url = "https://github.com/nixos/nixpkgs/tarball/nixos-unstable";
+#       sha256 = "sha256:0y1qzff8kiypijbbckyanlrginz62n5mpp8xsbs180flj58snzjg";
+#     })
+#     {
+#       system = pkgs.system;
+#       config = config.nixpkgs.config;
+#     };
+# in
 {
   imports = [
     ./hardware-configuration.nix
     ./tailscale.nix
-    # ./i3.nix
+    # ../../modules/virtualisation.nix
+    ./i3.nix
   ];
 
   programs.neovim = {
     enable = true;
-    package = unstable.neovim-unwrapped;
+    # package = unstable.neovim-unwrapped;
+    # package = pkgs.neovim;
     defaultEditor = true;
     vimAlias = true;
   };
