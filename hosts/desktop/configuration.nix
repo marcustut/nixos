426bfab822d42cedcdf80a46f51e8a391780e4f2
marcustut <marcustutorial@hotmail.com>
2025-05-08 15:18:05 +0800

    updated desktop config

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -8,14 +8,8 @@
     # GNOME configuration
     # ./gnome.nix
 
-    # Configuration for as a headscale server
-    ./headscale.nix
-
-    # Configuration for as a server
-    ./server.nix
-
-    # Configuration for a k3s cluster
-    ./k3s.nix
+    # VPN (tailscale)
+    ./tailscale.nix
 
     # Configuration for the minio server
     ./minio.nix
