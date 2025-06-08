f358f5b56bcfa030882164e8ab7ebbb6dd534864
marcustut <marcustutorial@hotmail.com>
2025-06-09 00:35:36 +0800

    feat: updated laptop and removed gateway

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,28 +1,16 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 
-# let
-#   unstable = import
-#     (builtins.fetchTarball {
-#       url = "https://github.com/nixos/nixpkgs/tarball/nixos-unstable";
-#       sha256 = "sha256:0y1qzff8kiypijbbckyanlrginz62n5mpp8xsbs180flj58snzjg";
-#     })
-#     {
-#       system = pkgs.system;
-#       config = config.nixpkgs.config;
-#     };
-# in
 {
   imports = [
     ./hardware-configuration.nix
     ./tailscale.nix
-    # ../../modules/virtualisation.nix
-    ./i3.nix
+    # ./i3.nix
+    ./gnome.nix
   ];
 
   programs.neovim = {
     enable = true;
-    # package = unstable.neovim-unwrapped;
-    # package = pkgs.neovim;
+    package = pkgs.unstable.neovim-unwrapped;
     defaultEditor = true;
     vimAlias = true;
   };
